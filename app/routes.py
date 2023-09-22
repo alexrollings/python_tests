2296ec341783664c8cfbbe29077b385164ef805a
Alexandra Rollings <alexandrarollings@Alexandras-MacBook-Pro.local>
2023-09-22 10:26:38 +0200

    Fix app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ def sum(arg):
     try: 
         total = 0
         for val in arg:
-            total -= val
+            total += val
     except Exception:
         return "Error occured!", 500
     return total
